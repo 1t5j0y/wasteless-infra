2c7636b42973aff9bd17a1992ee9d4f8aa0cae54
Joy <itsjoy@gmail.com>
2019-10-23 15:09:42 +0530

    fix leading spaces in IAM policy JSON

--- iam.tf
+++ iam.tf
@@ -3,20 +3,20 @@ resource "aws_iam_role" "lambda-iam-role" {
   name = "wasteless_lambda_role"
   
   assume_role_policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-          "Service": "lambda.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-      }
-    ]
-  }
-  EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
 
 #POLICIES
